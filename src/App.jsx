25121cc072e46bdb4cc04e234c93da5d907126d1
nyxt4s <garaynyxmedinagmail.com>
2024-04-04 21:21:47 -0300

    se agreganm cambios

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,21 @@ function App() {
           <Header></Header>
           <div className='main-banner'>
             <div>
-              <div><h1 className='main-banner-tittle'>ImagineCloud</h1></div>
-              <div>
+              <div className='main-banner-container'>
+                <h1 className='main-banner-tittle'>ImagineCloud</h1>
+              </div>
+              <div className='main-banner-container'>
                 <p className='main-banner-substittle'>
                     "Tu Tiempo Importa, Nuestro Tecnología
                     Simplifica: ¡Vive más, Preocúpate menos!"
                 </p>
               </div>
-              <div>
+              <div className='main-banner-container'>
                 <button className='main-banner-button'>Contactanos</button>
                 <button className='main-banner-button'>Conocenos</button>
               </div>
             </div>
-            <div>  <img src={programmer} alt="imagine Cloud" /></div>
+            <div>  <img src={programmer} alt="imagine Cloud" className='main-banner-image'/></div>
 
           </div>
 
